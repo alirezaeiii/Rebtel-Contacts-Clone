467791efef682aff733ffa3c517b13f1414cd9b7
Ali Rezaei <ali.rezaei09@gmail.com>
2020-01-14 18:46:54 +0330

    Reformat

--- app/src/main/java/com/sample/android/contact/widget/ResizingTextEditText.java
+++ app/src/main/java/com/sample/android/contact/widget/ResizingTextEditText.java
@@ -24,30 +24,31 @@ import android.util.AttributeSet;
 import com.sample.android.contact.R;
 import com.sample.android.contact.util.ViewUtil;
 
-/** EditText which resizes dynamically with respect to text length. */
+/**
+ * EditText which resizes dynamically with respect to text length.
+ */
 public class ResizingTextEditText extends AppCompatEditText {
 
-  private final int mOriginalTextSize;
-  private final int mMinTextSize;
+    private final int mOriginalTextSize;
+    private final int mMinTextSize;
 
-  public ResizingTextEditText(Context context, AttributeSet attrs) {
-    super(context, attrs);
-    mOriginalTextSize = (int) getTextSize();
-    TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ResizingText);
-    mMinTextSize =
-        (int) a.getDimension(R.styleable.ResizingText_resizing_text_min_size, mOriginalTextSize);
-    a.recycle();
-  }
+    public ResizingTextEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mOriginalTextSize = (int) getTextSize();
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ResizingText);
+        mMinTextSize = (int) a.getDimension(R.styleable.ResizingText_resizing_text_min_size, mOriginalTextSize);
+        a.recycle();
+    }
 
-  @Override
-  protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
-    super.onTextChanged(text, start, lengthBefore, lengthAfter);
-    ViewUtil.resizeText(this, mOriginalTextSize, mMinTextSize);
-  }
+    @Override
+    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        super.onTextChanged(text, start, lengthBefore, lengthAfter);
+        ViewUtil.resizeText(this, mOriginalTextSize, mMinTextSize);
+    }
 
-  @Override
-  protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-    super.onSizeChanged(w, h, oldw, oldh);
-    ViewUtil.resizeText(this, mOriginalTextSize, mMinTextSize);
-  }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        ViewUtil.resizeText(this, mOriginalTextSize, mMinTextSize);
+    }
 }
