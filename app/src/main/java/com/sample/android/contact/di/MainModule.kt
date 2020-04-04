b7fa4cc7fe0ae35b5fefbf68dd736da661ee9291
Ali Rezaei <ali.rezaei09@gmail.com>
2020-04-05 00:11:24 +0430

    Bind viewModel factory

--- app/src/main/java/com/sample/android/contact/di/MainModule.kt
+++ app/src/main/java/com/sample/android/contact/di/MainModule.kt
@@ -1,7 +1,9 @@
 package com.sample.android.contact.di
 
+import androidx.lifecycle.ViewModelProvider
 import com.sample.android.contact.ui.ContactsFragment
 import com.sample.android.contact.ui.DialpadFragment
+import com.sample.android.contact.viewmodels.ContactsViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -14,4 +16,7 @@ abstract class MainModule {
 
     @ContributesAndroidInjector
     internal abstract fun dialpadFragment(): DialpadFragment
+
+    @Binds
+    internal abstract fun bindViewModelFactory(factory: ContactsViewModel.Factory): ViewModelProvider.Factory
 }
