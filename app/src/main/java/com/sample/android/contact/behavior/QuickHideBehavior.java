e947207cd3121d929e6ce5b2e2e1a37e4524c750
Ali Rezaei <ali.rezaei09@gmail.com>
2020-07-16 23:05:00 +0430

    Revert

--- app/src/main/java/com/sample/android/contact/behavior/QuickHideBehavior.java
+++ app/src/main/java/com/sample/android/contact/behavior/QuickHideBehavior.java
@@ -2,15 +2,13 @@ package com.sample.android.contact.behavior;
 
 import android.animation.ObjectAnimator;
 import android.content.Context;
-
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import androidx.core.view.ViewCompat;
-
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.sample.android.contact.R;
+import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.ViewCompat;
 
 /**
  * Simple scrolling behavior that monitors nested events in the scrolling
@@ -26,11 +24,11 @@ public abstract class QuickHideBehavior extends CoordinatorLayout.Behavior<View>
 
     private ObjectAnimator mAnimator;
 
-    private View mRecyclerView;
+    protected View mRecyclerView;
 
-    protected abstract void directionUpScrolling(View recyclerView);
+    protected abstract void directionUpScrolling();
 
-    protected abstract void directionDownScrolling(View recyclerView);
+    protected abstract void directionDownScrolling();
 
     protected abstract float getTargetHideValue(ViewGroup parent, View target);
 
@@ -47,32 +45,29 @@ public abstract class QuickHideBehavior extends CoordinatorLayout.Behavior<View>
 
     //Called before a nested scroll event. Return true to declare interest
     @Override
-    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
-                                       View child, View directTargetChild, View target,
-                                       int nestedScrollAxes, int type) {
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
+                                       @NonNull View child, @NonNull View directTargetChild,
+                                       @NonNull View target, int nestedScrollAxes, int type) {
         //We have to declare interest in the scroll to receive further events
         return (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
     }
 
     //Called after the scrolling child handles the fling
     @Override
-    public boolean onNestedFling(CoordinatorLayout coordinatorLayout,
-                                 View child, View target, float velocityX, float velocityY,
-                                 boolean consumed) {
-        if(mRecyclerView == null) {
-            mRecyclerView = target.findViewById(R.id.recyclerView);
-        }
+    public boolean onNestedFling(@NonNull CoordinatorLayout coordinatorLayout,
+                                 @NonNull View child, @NonNull View target, float velocityX,
+                                 float velocityY, boolean consumed) {
         //We only care when the target view is already handling the fling
         if (consumed) {
             if (velocityY > 0 && mScrollTrigger != DIRECTION_UP) {
                 mScrollTrigger = DIRECTION_UP;
                 restartAnimator(child, getTargetHideValue(coordinatorLayout, child));
-                directionUpScrolling(mRecyclerView);
+                directionUpScrolling();
 
             } else if (velocityY < 0 && mScrollTrigger != DIRECTION_DOWN) {
                 mScrollTrigger = DIRECTION_DOWN;
                 restartAnimator(child, 0f);
-                directionDownScrolling(mRecyclerView);
+                directionDownScrolling();
             }
         }
         return false;
@@ -81,12 +76,11 @@ public abstract class QuickHideBehavior extends CoordinatorLayout.Behavior<View>
     /* Helper Methods */
 
     //Helper to trigger hide/show animation
-    private void restartAnimator(View target, float value) {
+    protected void restartAnimator(View target, float value) {
         if (mAnimator != null) {
             mAnimator.cancel();
             mAnimator = null;
         }
-
         mAnimator = ObjectAnimator
                 .ofFloat(target, View.TRANSLATION_Y, value)
                 .setDuration(250);
--- app/src/main/java/com/sample/android/contact/behavior/QuickHideBottomBarBehavior.java
+++ app/src/main/java/com/sample/android/contact/behavior/QuickHideBottomBarBehavior.java
@@ -8,12 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.MarginLayoutParams;
 
+import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
 import com.sample.android.contact.R;
 
 public class QuickHideBottomBarBehavior extends QuickHideBehavior {
 
     private Handler mHandler = new Handler(Looper.getMainLooper());
-
     private int bottomSpacing;
 
     //Required to instantiate as a default behavior
@@ -29,23 +31,33 @@ public class QuickHideBottomBarBehavior extends QuickHideBehavior {
     }
 
     @Override
-    protected float getTargetHideValue(ViewGroup parent, View target) {
-        return parent.getHeight() - target.getTop();
+    public boolean onNestedFling(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child,
+                                 @NonNull View target, float velocityX, float velocityY,
+                                 boolean consumed) {
+        if(mRecyclerView == null) {
+            mRecyclerView = target.findViewById(R.id.recyclerView);
+        }
+        return super.onNestedFling(coordinatorLayout, child, target, velocityX, velocityY, consumed);
     }
 
     @Override
-    protected void directionUpScrolling(View recyclerView) {
-        MarginLayoutParams params = (MarginLayoutParams) recyclerView.getLayoutParams();
+    protected void directionUpScrolling() {
+        MarginLayoutParams params = (MarginLayoutParams) mRecyclerView.getLayoutParams();
         params.setMargins(0, 0, 0, 0);
-        recyclerView.setLayoutParams(params);
+        mRecyclerView.setLayoutParams(params);
     }
 
     @Override
-    protected void directionDownScrolling(View recyclerView) {
-        MarginLayoutParams params = (MarginLayoutParams) recyclerView.getLayoutParams();
+    protected void directionDownScrolling() {
+        MarginLayoutParams params = (MarginLayoutParams) mRecyclerView.getLayoutParams();
         mHandler.postDelayed(() -> {
             params.setMargins(0, 0, 0, bottomSpacing);
-            recyclerView.setLayoutParams(params);
+            mRecyclerView.setLayoutParams(params);
         }, 250);
+    }
+
+    @Override
+    protected float getTargetHideValue(ViewGroup parent, View target) {
+        return parent.getHeight() - target.getTop();
     }
 }
