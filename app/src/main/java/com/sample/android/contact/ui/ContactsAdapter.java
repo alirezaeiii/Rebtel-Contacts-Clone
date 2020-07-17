54d82fc37da547b825422b0ff8e10302a81dc984
Ali Rezaei <ali.rezaei09@gmail.com>
2020-07-18 01:06:00 +0430

    Cleanup

--- app/src/main/java/com/sample/android/contact/ui/ContactsAdapter.java
+++ app/src/main/java/com/sample/android/contact/ui/ContactsAdapter.java
@@ -2,13 +2,6 @@ package com.sample.android.contact.ui;
 
 import android.content.Context;
 import android.os.Handler;
-
-import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
-import androidx.recyclerview.widget.LinearSmoothScroller;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Looper;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
@@ -19,6 +12,12 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.recyclerview.widget.LinearSmoothScroller;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.sample.android.contact.R;
 import com.sample.android.contact.domain.Contact;
@@ -120,18 +119,8 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHo
         }
 
         void bind(Contact contact) {
-
             Context context = mRecyclerView.getContext();
             final int position = getAdapterPosition();
-
-            String name = contact.getName();
-            List<ContactPhoneNumber> numbers = contact.getPhoneNumbers();
-            ContactPhoneNumber phoneNumber = numbers.get(0);
-            String number = numbers.size() == 1 ? phoneNumber.getNumber() : "";
-
-            contactNameView.setText(name);
-            imageText.setText(contact.getBriefName());
-            phoneNumberView.setText(number);
 
             flagItem.removeAllViews();
             for (int flagResId : contact.getFlagResIds()) {
@@ -145,30 +134,9 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHo
                 flagItem.addView(imageView);
             }
 
-            ConstraintSet constraintSet = new ConstraintSet();
-            int viewId;
-            if (numbers.size() == 1) {
-                phoneNumberType.setVisibility(View.VISIBLE);
-                lineNumber.setVisibility(View.INVISIBLE);
-                phoneNumberType.setText(phoneNumber.getTypeLabel());
-                constraintSet.clone(detail);
-                viewId = R.id.phone_type;
-            } else {
-                lineNumber.setVisibility(View.VISIBLE);
-                phoneNumberType.setVisibility(View.INVISIBLE);
-                lineNumber.setText(String.valueOf(numbers.size()));
-                constraintSet.clone(detail);
-                viewId = R.id.line_number;
-            }
-            constraintSet.connect(R.id.contact_name,
-                    ConstraintSet.END,
-                    viewId,
-                    ConstraintSet.START);
-            constraintSet.applyTo(detail);
-
             if (mShowSeparator) {
                 ContactSeparator contactSeparator = contact.getContactSeparator();
-                if (contactSeparator!= null && contactSeparator.getShowSeparator()) {
+                if (contactSeparator != null && contactSeparator.getShowSeparator()) {
                     separatorText.setText(String.valueOf(contactSeparator.getSeparatorChar()));
                     separatorView.setVisibility(View.VISIBLE);
                 } else {
@@ -183,63 +151,84 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHo
                 separatorView.setVisibility(View.GONE);
                 line.setVisibility(View.VISIBLE);
             }
-
             subItem.removeAllViews();
+            String name = contact.getName();
+            List<ContactPhoneNumber> numbers = contact.getPhoneNumbers();
+            String number = "";
+            ConstraintSet constraintSet = new ConstraintSet();
+            int viewId;
             if (numbers.size() == 1) {
-                return;
-            }
-
-            boolean lineFlag = true;
-            if (mShowSeparator) {
-                lineFlag = position == mContacts.size() - 1 ? false : contact.getLineFlag();
-            }
-
-            boolean expanded = contact.isExpanded();
-            subItem.setVisibility(expanded ? View.VISIBLE : View.GONE);
-
-            for (int childPosition = 0; childPosition < numbers.size(); childPosition++) {
-
-                phoneNumber = numbers.get(childPosition);
-
-                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                View childView = inflater.inflate(R.layout.child_item, null);
-
-                ChildViewHolder childViewHolder = new ChildViewHolder(childView);
-
-                childViewHolder.contactNumber.setText(phoneNumber.getNumber());
-                childViewHolder.numberType.setText(phoneNumber.getTypeLabel());
-                childViewHolder.flagImageView.setImageResource(phoneNumber.getFlagResId());
-
-                childViewHolder.childLine.setVisibility(lineFlag ? View.VISIBLE : View.GONE);
-                childViewHolder.childTopLine.setVisibility(lineFlag ? View.GONE : View.VISIBLE);
-
-                FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(
-                        FrameLayout.LayoutParams.WRAP_CONTENT,
-                        FrameLayout.LayoutParams.WRAP_CONTENT);
-
-                FrameLayout.LayoutParams rlp = new FrameLayout.LayoutParams(
-                        RelativeLayout.LayoutParams.WRAP_CONTENT,
-                        RelativeLayout.LayoutParams.WRAP_CONTENT);
-
-                if (lineFlag) {
-                    lp.setMarginStart((int) context.getResources().getDimension(R.dimen.dimen_frame_margin_default));
-                    childViewHolder.frameLayout.setLayoutParams(lp);
-
-                    rlp.setMarginStart((int) context.getResources().getDimension(R.dimen.dimen_relative_margin_default));
-                    childViewHolder.relativeLayout.setLayoutParams(rlp);
-                } else {
-                    if (childPosition == 0) {
-                        lp.setMarginStart((int) context.getResources().getDimension(R.dimen.dimen_frame_margin_default));
-                        rlp.setMarginStart((int) context.getResources().getDimension(R.dimen.dimen_relative_margin_default));
-                    } else {
-                        lp.setMarginStart((int) context.getResources().getDimension(R.dimen.dimen_frame_margin));
-                        rlp.setMarginStart(0);
-                    }
-                    childViewHolder.frameLayout.setLayoutParams(lp);
-                    childViewHolder.relativeLayout.setLayoutParams(rlp);
+                ContactPhoneNumber phoneNumber = numbers.get(0);
+                number = phoneNumber.getNumber();
+                phoneNumberType.setVisibility(View.VISIBLE);
+                lineNumber.setVisibility(View.INVISIBLE);
+                phoneNumberType.setText(phoneNumber.getTypeLabel());
+                viewId = R.id.phone_type;
+            } else {
+                lineNumber.setVisibility(View.VISIBLE);
+                phoneNumberType.setVisibility(View.INVISIBLE);
+                lineNumber.setText(String.valueOf(numbers.size()));
+                viewId = R.id.line_number;
+                boolean lineFlag = true;
+                if (mShowSeparator) {
+                    lineFlag = position != mContacts.size() - 1 && contact.getLineFlag();
                 }
-                subItem.addView(childView);
+                boolean expanded = contact.isExpanded();
+                subItem.setVisibility(expanded ? View.VISIBLE : View.GONE);
+
+                for (int childPosition = 0; childPosition < numbers.size(); childPosition++) {
+
+                    ContactPhoneNumber phoneNumber = numbers.get(childPosition);
+
+                    LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    View childView = inflater.inflate(R.layout.child_item, null);
+
+                    ChildViewHolder childViewHolder = new ChildViewHolder(childView);
+
+                    childViewHolder.contactNumber.setText(phoneNumber.getNumber());
+                    childViewHolder.numberType.setText(phoneNumber.getTypeLabel());
+                    childViewHolder.flagImageView.setImageResource(phoneNumber.getFlagResId());
+
+                    childViewHolder.childLine.setVisibility(lineFlag ? View.VISIBLE : View.GONE);
+                    childViewHolder.childTopLine.setVisibility(lineFlag ? View.GONE : View.VISIBLE);
+
+                    FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(
+                            FrameLayout.LayoutParams.WRAP_CONTENT,
+                            FrameLayout.LayoutParams.WRAP_CONTENT);
+
+                    FrameLayout.LayoutParams rlp = new FrameLayout.LayoutParams(
+                            RelativeLayout.LayoutParams.WRAP_CONTENT,
+                            RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+                    if (lineFlag) {
+                        lp.setMarginStart((int) context.getResources().getDimension(R.dimen.dimen_frame_margin_default));
+                        childViewHolder.frameLayout.setLayoutParams(lp);
+
+                        rlp.setMarginStart((int) context.getResources().getDimension(R.dimen.dimen_relative_margin_default));
+                        childViewHolder.relativeLayout.setLayoutParams(rlp);
+                    } else {
+                        if (childPosition == 0) {
+                            lp.setMarginStart((int) context.getResources().getDimension(R.dimen.dimen_frame_margin_default));
+                            rlp.setMarginStart((int) context.getResources().getDimension(R.dimen.dimen_relative_margin_default));
+                        } else {
+                            lp.setMarginStart((int) context.getResources().getDimension(R.dimen.dimen_frame_margin));
+                            rlp.setMarginStart(0);
+                        }
+                        childViewHolder.frameLayout.setLayoutParams(lp);
+                        childViewHolder.relativeLayout.setLayoutParams(rlp);
+                    }
+                    subItem.addView(childView);
+                }
             }
+            constraintSet.clone(detail);
+            constraintSet.connect(R.id.contact_name,
+                    ConstraintSet.END,
+                    viewId,
+                    ConstraintSet.START);
+            constraintSet.applyTo(detail);
+            contactNameView.setText(name);
+            imageText.setText(contact.getBriefName());
+            phoneNumberView.setText(number);
         }
 
         @OnClick(R.id.detail)
