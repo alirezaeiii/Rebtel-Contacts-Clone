5d703558a8765aea879cdb437368f8471a874c83
Ali Rezaei <ali.rezaei09@gmail.com>
2020-08-30 22:39:01 +0430

    Load contacts in ViewModel

--- app/src/main/java/com/sample/android/contact/viewmodels/ContactsViewModel.kt
+++ app/src/main/java/com/sample/android/contact/viewmodels/ContactsViewModel.kt
@@ -3,8 +3,8 @@ package com.sample.android.contact.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.sample.android.contact.repository.ContactsRepository
 import com.sample.android.contact.domain.Contact
+import com.sample.android.contact.repository.ContactsRepository
 import com.sample.android.contact.util.Resource
 import javax.inject.Inject
 
@@ -13,6 +13,12 @@ class ContactsViewModel(repository: ContactsRepository) : ViewModel() {
     private val _liveData = repository.liveData
     val liveData: LiveData<Resource<List<Contact>>>
         get() = _liveData
+
+    init {
+        if(repository.liveData.value == null) {
+            repository.loadContacts()
+        }
+    }
 
     /**
      * Factory for constructing ContactsViewModel with parameter
