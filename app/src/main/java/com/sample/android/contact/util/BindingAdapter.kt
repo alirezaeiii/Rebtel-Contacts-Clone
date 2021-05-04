5e67ec2807daadd0b0e5718a47e586c70b39a210
Ali Rezaei <ali.rezaei09@gmail.com>
2021-05-05 01:05:40 +0430

    Modification

--- app/src/main/java/com/sample/android/contact/util/BindingAdapter.kt
+++ app/src/main/java/com/sample/android/contact/util/BindingAdapter.kt
@@ -5,12 +5,12 @@ import android.widget.ProgressBar
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("showLoading")
-fun ProgressBar.showLoading(resource: Resource<*>?) {
+fun <T> ProgressBar.showLoading(resource: Resource<T>?) {
     visibility = if (resource is Resource.Loading) View.VISIBLE else View.GONE
 }
 
 
 @BindingAdapter("showData")
-fun View.showData(resource: Resource<*>?) {
+fun <T> View.showData(resource: Resource<T>?) {
     visibility = if (resource is Resource.Success) View.VISIBLE else View.GONE
 }
